ba8e291ff1448ca9131e4f9016e840cde7ffbe96
Iskander Hughes <iskanderhughes@hotmail.com>
2023-02-25 17:45:54 +0000

    validations added

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,2 +1,7 @@
 class Trip < ApplicationRecord
+  validates :title, presence: true
+  validates :start_point, presence: true
+  validates :end_point, presence: true
+  validates :summary, length: { in: 16..100 }
+  validates :region, presence: true
 end
