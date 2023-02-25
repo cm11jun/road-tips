ea8d2b9cfcec3489edec110998ab0af848b2bff1
Chris Moore <chris@deskhive.co.uk>
2023-02-25 17:40:12 +0000

    done

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,18 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :my_trips, only: [:index]
   resources :trips
-  resources :features, only: [:index, :show]
-  resources :trip_features, only: []
+  resources :pois, only: [:index, :show] do
+    resources :reviews, only: [:new, :create]
+    resources :my_trips, only: [:new, :create, :show]
+  end
+  resources :trip_pois, only: []
+  resources :reviews, only: [:destroy]
+  resources :users, only: [] do
+    resources :my_trips, only: [:index]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
+
