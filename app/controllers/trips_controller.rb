595fcae07c7d141d43a3bb420cfc4825dbe7873c
Jaimie Irvine <jaimie.irvine1990@gmail.com>
2023-02-28 21:20:12 +0000

    add create and new icons

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -7,6 +7,39 @@ class TripsController < ApplicationController
     @trip = Trip.find(params[:id])
   end
 
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+    # @trip.user = current_user
+    if @trip.save
+      redirect_to new_trip_path(@trip), notice: "Trip created!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @trip = Trip.find(params[:id])
+  end
+
+  def update
+    @trip = Trip.find(params[:id])
+    if @trip.update(trip_params)
+      redirect_to @trip, notice: "Trip updated!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @trip = Trip.find(params[:id])
+    @trip.destroy
+    redirect_to trips_path, status: :see_other
+  end
+
   private
 
   def trip_params
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,2 +1,8 @@
 class Trip < ApplicationRecord
+  # has_many trip_pois
+  validates :title, presence: true
+  validates :start_point, presence: true
+  validates :end_point, presence: true
+  validates :summary, length: { in: 10..500 }
+  validates :region, presence: true
 end
