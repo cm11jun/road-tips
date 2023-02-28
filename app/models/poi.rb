c832460f748a28c13fc0cc268f0f5085612578c5
Iskander Hughes <iskanderhughes@hotmail.com>
2023-02-28 21:11:59 +0000

    poi seeds added

--- app/models/poi.rb
+++ app/models/poi.rb
@@ -1,5 +1,7 @@
 class Poi < ApplicationRecord
+  has_many :trip_pois
   validates :name, presence: true, uniqueness: true
-  validates :type, inclusion: { in: ["technical/fun roads", "eats", "sleeps", "scenic roads", "landmarks"] }
-  validates :description, length: { in: 16..100 }
+  validates :type, inclusion: { in: ["technical/fun roads", "eats", "sleeps", "scenic roads", "landmarks", "services"] }
+  validates :description, length: { in: 16..256 }
+  validates :address, presence: true, uniqueness: true
 end
