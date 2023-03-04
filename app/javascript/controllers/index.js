fda4af557d6bef34fe61c1efe34f398f0bcc9740
Iskander Hughes <iskanderhughes@hotmail.com>
2023-03-04 15:19:49 +0000

    mapping software somewhat installed

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,6 @@ application.register("hello", HelloController)
 
 import StarRatingController from "./star_rating_controller"
 application.register("star-rating", StarRatingController)
+
+import TripShowMapController from "./trip_show_map_controller"
+application.register("trip_show_map", TripShowMapController)
