f6a2fdcbb62effe1c2e942e4ae1d02be1fbdbfe3
Iskander Hughes <iskanderhughes@hotmail.com>
2023-03-18 16:13:43 +0000

    still some broken bits

--- app/javascript/controllers/tom_select_controller.js
+++ app/javascript/controllers/tom_select_controller.js
@@ -5,7 +5,7 @@ export default class extends Controller {
 
   connect() {
     console.log("?");
-    const select = document.querySelector(".hahahah");
+    const select = document.querySelector(".stops");
 
     new TomSelect(select,{
       maxItems: 10,
