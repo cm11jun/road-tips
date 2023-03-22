a16a2512fd5bed00a72c2984f1c9905c704890d3
Jaimie Irvine <jaimie.irvine1990@gmail.com>
2023-03-22 00:09:20 +0000

    seed images to pois and display on map

--- app/models/poi.rb
+++ app/models/poi.rb
@@ -2,6 +2,7 @@ class Poi < ApplicationRecord
   geocoded_by :address
   has_many :trip_pois
   has_many :trips, :through => :trip_pois
+  has_one_attached :photo
   after_validation :geocode, if: :will_save_change_to_address?
 
 
